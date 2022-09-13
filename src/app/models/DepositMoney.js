7e39789d9db162c01923b03bcb302e4c1f209985
Henrique Hoinacki <riqueah@gmail.com>
2022-09-13 15:26:13 -0300

    new implementations

---
+++ src/app/models/DepositMoney.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+
+class DepositMoney extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        financial_statements_id: Sequelize.INTEGER,
+        type_transaction: Sequelize.STRING,
+        local: Sequelize.STRING,
+        type_bank: Sequelize.STRING,
+        value: Sequelize.DOUBLE,
+      },
+      {
+        sequelize,
+        timestamps: true,
+      }
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.FinancialStatements, { foreignKey: 'financial_statements_id', as: 'financialStatements' });
+  }
+}
+
+export default DepositMoney;
---
+++ src/app/models/TravelExpenses.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+
+class TravelExpenses extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        financial_statements_id: Sequelize.INTEGER,
+        type_establishment: Sequelize.STRING,
+        name_establishment: Sequelize.STRING,
+        expense_description: Sequelize.STRING,
+        value: Sequelize.DOUBLE,
+      },
+      {
+        sequelize,
+        timestamps: true,
+      }
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.FinancialStatements, { foreignKey: 'financial_statements_id', as: 'financialStatements' });
+  }
+}
+
+export default TravelExpenses;
