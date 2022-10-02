b99ceb462c61e759f4785eb97b8bf3ad3fd1f6c4
Henrique Hoinacki <riqueah@gmail.com>
2022-10-02 11:21:18 -0300

    .

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 
 import SessionController from './app/controller/SessionController';
-import DriverController from './app/controller/DriverController';
 import FreightController from './app/controller/FreightController';
 import DepositMoneyController from './app/controller/DepositMoneyController';
 import TravelExpensesController from './app/controller/TravelExpensesController';
@@ -14,31 +13,21 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 
 // cadastro
-routes.post('/driver/register', DriverController.createDriver)
 routes.post('/driver/authenticate', SessionController.sessioDriver);
 
 // autenticação
 routes.use(authMiddleware);
 
-// users driver
-routes.put('/driver/:id', DriverController.updateDriver)
-      .get('/driver/:id', DriverController.getIdDriver)
-      .get('/drivers', DriverController.getAllDriver)
-      .delete('/driver/:id', DriverController.deleteDriver);
-
 // financial statements
-routes.post('/financialStatement', FinancialStatementsController.createFinancialStatements)
-      .put('/financialStatement/:id', FinancialStatementsController.updateFinancialStatements)
-      .get('/financialStatement/:id', FinancialStatementsController.getIdFinancialStatements)
-      .get('/financialStatements', FinancialStatementsController.getAllFinancialStatements)
-      .delete('/financialStatement/:id', FinancialStatementsController.deleteFinancialStatements);
+routes.put('/financialStatement/:id', FinancialStatementsController.updateFinancialStatements)
+      .get('/financialStatement/:id', FinancialStatementsController.getIdFinancialStatements);
 
 // freight
 routes.post('/freight', FreightController.createFreight)
       .put('/freight/:id', FreightController.updateFreight)
       .get('/freight/:id', FreightController.getIdFreight)
       .get('/freights', FreightController.getAllFreight)
-      .delete('/freight/:id', FreightController.deleteFreight)
+      .delete('/freight/:id', FreightController.deleteFreight);
 
 // notification
 routes.get('/notifications', NotificationController.getAllNotification);
