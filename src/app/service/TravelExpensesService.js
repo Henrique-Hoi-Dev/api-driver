a9aadaaac857c6b43004c4d32597513e21776e8e
Henrique Hoinacki <henrique.hoinacki@ilia.digital>
2023-08-19 15:05:42 -0300

    erro tag

--- src/app/service/TravelExpensesService.js
+++ src/app/service/TravelExpensesService.js
@@ -9,10 +9,10 @@ export default {
     const financial = await FinancialStatements.findOne({
       where: { driver_id: driverId, status: true },
     });
-    if (!financial) throw Error('Financial statements not found');
+    if (!financial) throw Error('FINANCIAL_NOT_FOUND');
 
     const freight = await Freight.findByPk(freight_id);
-    if (!freight) throw Error('Freight not found');
+    if (!freight) throw Error('FREIGHT_NOT_FOUND');
 
     if (freight.status === 'STARTING_TRIP') {
       const result = await TravelExpenses.create({
